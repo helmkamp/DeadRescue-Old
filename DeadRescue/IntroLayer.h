d59bb892f852d74b8ade60741a1c75e123de8af3
Andrew Helmkamp <helmy@threebeersaday.com>
2012-12-17 14:22:23 -0600

    Initial Commit

---
+++ DeadRescue/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  DeadRescue
+//
+//  Created by Andrew Helmkamp on 12/17/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
