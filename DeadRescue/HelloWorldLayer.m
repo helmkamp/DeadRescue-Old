b1531ab536ee60b0fe980ef8a6f8133a5056955b
Andrew Helmkamp <helmy@threebeersaday.com>
2012-12-21 23:23:59 -0600

    last commit

--- DeadRescue/HelloWorldLayer.m
+++ DeadRescue/HelloWorldLayer.m
@@ -168,7 +168,7 @@
     touchLocation = [[CCDirector sharedDirector] convertToGL: touchLocation];
     touchLocation = [self convertToNodeSpace:touchLocation];
     
-    _player.position = touchLocation;
+   // _player.position = touchLocation;
     
     CGPoint playerPos = _player.position;
     CGPoint diff = ccpSub(touchLocation, playerPos);
